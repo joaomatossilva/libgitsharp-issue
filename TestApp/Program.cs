1de8c8013f58329c7ceda3c99f36af439fa83c77
Joao Matos Silva <joaomatos.silva@farfetch.com>
2019-09-30 10:43:11 +0100

    adding test solution

---
+++ TestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TestApp
+{
+    using LibGit2Sharp;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var repo = new Repository("."))
+            {
+
+                var remoteRefName = "refs/remotes/origin/master";
+                var branchName = "master";
+
+                var remoteBranch = repo.Branches[remoteRefName];
+
+                var localBranch = repo.Branches[branchName];
+                if (localBranch == null)
+                {
+                    localBranch = repo.CreateBranch(branchName, remoteBranch.Tip);
+                }
+
+                repo.Branches.Update(localBranch, b =>
+                {
+                    b.TrackedBranch = remoteBranch.CanonicalName;
+                });
+            }
+        }
+    }
+}
